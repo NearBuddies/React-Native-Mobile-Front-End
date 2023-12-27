588fe731ed0baf51798169db440bb2ab01c3c380
MamaneHassane <hassane.mamane017@gmail.com>
2023-12-27 16:59:22 +0100

    ongoing

--- App.js
+++ App.js
@@ -10,17 +10,53 @@ import UserMainPage from './Components/User/UserMainPage';
 import UserExplore from './Components/User/UserExplore';
 import UserForYou from './Components/User/UserForYou';
 import CustomAppBar from './Components/Common/CustomAppBar';
+import UserNavigation from './Components/User/UserNavigation';
 import Settings from './Components/Common/Settings';
 
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <>
     <CustomAppBar />
     <NavigationContainer theme={MyTheme}>
-      
-
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen
+          name="Home"
+          component={UserNavigation}
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/icons/house.png')}
+                style={{ width: 20, height: 20 }} />
+            )
+          }} />
+        <Stack.Screen
+          name="For You"
+          component={UserForYou}
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/icons/for-you.png')}
+                style={{ width: 20, height: 20 }} />
+            )
+          }} />
+        <Stack.Screen
+          name="Explore"
+          component={UserExplore}
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/icons/compass.png')}
+                style={{ width: 20, height: 20 }} />
+            )
+          }} />
+        <Stack.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarIcon: () => (
+              <Image source={require('./assets/icons/tools.png')}
+                style={{ width: 20, height: 20 }} />
+            )
+          }} />
+      </Stack.Navigator>
     </NavigationContainer></>
         
   );
--- Components/User/UserNavigation.jsx
+++ Components/User/UserNavigation.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import { MyTheme } from '../../Variables';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer, DefaultTheme, useLinking } from '@react-navigation/native';
+import CustomAppBar from '../Common/CustomAppBar';
+import Settings from '../Common/Settings';
 import UserMainPage from './UserMainPage';
 import UserExplore from './UserExplore';
 import UserForYou from './UserForYou';
 export default function UserNavigation(){
-
     const Tab = createBottomTabNavigator();
     return(
         <>
         <CustomAppBar />
-        <NavigationContainer theme={MyTheme}>
           <Tab.Navigator screenOptions={{ headerShown: false }}>
             <Tab.Screen
               name="Home"
@@ -46,11 +47,11 @@ export default function UserNavigation(){
               component={Settings}
               options={{
                 tabBarIcon: () => (
-                  <Image source={require('./assets/icons/tools.png')}
+                  <Image source={require('../../assets/icons/tools.png')}
                     style={{ width: 20, height: 20 }} />
                 )
               }} />
           </Tab.Navigator>
-        </NavigationContainer></>
+        </>
     )  
 }
