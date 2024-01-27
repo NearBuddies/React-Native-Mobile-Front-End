113012c81f9a5f7417689a2383a6b94eec3cf385
MFR096 <hananemfr096@gmail.com>
2024-01-27 09:04:38 +0100

    event page added

--- Components/Event/NewEvent.jsx
+++ Components/Event/NewEvent.jsx
@@ -2,30 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { Button, Pressable, StyleSheet, Text, View, TextInput, Image, Alert, TouchableOpacity, Dimensions, } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useNavigation } from '@react-navigation/native';
-import {Feather} from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import * as Location from 'expo-location';
 import Modal from "react-native-modal";
 export default function NewEvent({
 
 }) {
-   
+
     // Se mettre une navigation
     const navigation = useNavigation();
     // Conserver l'image de la communauté
-    const [ image, setImage ] = useState(null);
+    const [image, setImage] = useState(null);
     // Fonction de selection de l'image
     const pickImage = async () => {
         // No permissions request is necessary for launching the image library
         let result = await ImagePicker.launchImageLibraryAsync({
-          mediaTypes: ImagePicker.MediaTypeOptions.All,
-          allowsEditing: true,
-          aspect: [4, 3],
-          quality: 1,
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
         });
         console.log(result);
         if (!result.canceled) {
-          setImage(result.assets[0].uri);
+            setImage(result.assets[0].uri);
         }
     };
     // Fonction pour recommencer la sélection d'image
@@ -42,284 +42,305 @@ export default function NewEvent({
         setModalVisible(!isModalVisible);
     };
 
-    const windowWidth = Dimensions.get("window").width;
-    const windowHeight = Dimensions.get("window").height;
+    const handleMapPress = (event) => {
+        const { coordinate } = event.nativeEvent;
+        setSelectedLocation(coordinate);
+    };
+
+    const handleLocationConfirmation = () => {
+        console.log("Selected Location:", selectedLocation);
+        // Add logic to save the location data, e.g., in state or AsyncStorage
+        // ...
+        toggleModal();
+    };
+
 
     const [currentLocation, setCurrentLocation] = useState(null);
+    const [selectedLocation, setSelectedLocation] = useState(null);
     const [initialRegion, setInitialRegion] = useState(null);
 
     useEffect(() => {
         const getLocation = async () => {
-        let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-            console.log("Permission to access location was denied");
-            return;
-        }
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+                console.log("Permission to access location was denied");
+                return;
+            }
 
-        let location = await Location.getCurrentPositionAsync({});
-        setCurrentLocation(location.coords);
+            let location = await Location.getCurrentPositionAsync({});
+            setCurrentLocation(location.coords);
 
-        setInitialRegion({
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-            latitudeDelta: 0.005,
-            longitudeDelta: 0.005,
-        });
+            setInitialRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            });
         };
 
         getLocation();
+        const reverseGeocodeLocation = async () => {
+            const { latitude, longitude } = location;
+            const results = await Location.reverseGeocodeAsync({ latitude, longitude });
+            console.log(`Reverse Geocoded Address: ${results[0]}`);
+        };
+        reverseGeocodeLocation();
     }, []);
 
     // Rendering de la page
     return (
-        <View style={styles.parentView}> 
+        <View style={styles.parentView}>
             <View style={styles.titlesView}>
                 <Image source={require("../../assets/images/NearBuddies_logo.jpg")}
-                style={styles.titlesImage}
+                    style={styles.titlesImage}
                 />
                 <Text style={styles.createCommunityOrEventTextStyle}>New Event</Text>
             </View>
-            <Text style = {styles.enhanceNewCommunityOrEventTextStyle}>Meet your community and earn credits!</Text>
+            <Text style={styles.enhanceNewCommunityOrEventTextStyle}>Meet your community and earn credits!</Text>
             <View style={styles.centralView}>
                 <View style={styles.inputView}>
                     <Pressable>
-                        <Feather name="type" size={24} color="#ec6a6d" style = {styles.inputIcons}/> 
+                        <Feather name="type" size={24} color="#ec6a6d" style={styles.inputIcons} />
                     </Pressable>
-                    <TextInput style={styles.inputTextInput} placeholder="Title"/>
+                    <TextInput style={styles.inputTextInput} placeholder="Title" />
                 </View>
                 <View style={styles.inputView}>
                     <Pressable>
-                        <Feather name="align-justify" size={24} color="#ec6a6d" style = {styles.inputIcons}/> 
+                        <Feather name="align-justify" size={24} color="#ec6a6d" style={styles.inputIcons} />
                     </Pressable>
-                    <TextInput style={styles.inputTextInput} placeholder="Description"/>
+                    <TextInput style={styles.inputTextInput} placeholder="Description" />
                 </View>
-                <View  style={styles.inputView}>
+                <View style={styles.inputView}>
                     <Pressable onPress={toggleModal}>
-                        <Feather name="map" size={24} color="#ec6a6d" style = {styles.inputIcons}/>
+                        <Feather name="map" size={24} color="#ec6a6d" style={styles.inputIcons} />
                     </Pressable>
-                    <TextInput style={styles.inputTextInput} placeholder="Location"/>
+                    <TextInput style={styles.inputTextInput} placeholder="Location" />
                     <Modal isVisible={isModalVisible}>
                         <View style={{ flex: 1 }}>
                             <MapView
-                                style={StyleSheet.absoluteFill}
-                                provider= {PROVIDER_GOOGLE}
+                                style={{ height: '40%' }}
+                                provider={PROVIDER_GOOGLE}
                                 initialRegion={initialRegion}
                                 showsUserLocation
                                 showsMyLocationButton
+                                onPress={handleMapPress}
                             >
-                                {currentLocation && (
-                                    <Marker
-                                    coordinate={{
-                                        latitude: currentLocation.latitude,
-                                        longitude: currentLocation.longitude,
-                                    }}
-                                    title="Your Location"
-                                    />
+                                {selectedLocation ? (
+                                    <Marker coordinate={selectedLocation} title="Selected Location" />
+                                ) : (
+                                    currentLocation && (
+                                        <Marker
+                                            coordinate={{
+                                                latitude: currentLocation.latitude,
+                                                longitude: currentLocation.longitude,
+                                            }}
+                                            title="Your are here"
+                                        />
+                                    )
                                 )}
                             </MapView>
-
-                        <Button title="Hide map" onPress={toggleModal} />
+                            <Button title="Confirm Location" onPress={handleLocationConfirmation} />
                         </View>
                     </Modal>
                 </View>
                 <View style={styles.uploadImageView}>
-                        {
-                            !image
-                             &&                             
-                            <Pressable style={styles.uploadImagePressable} onPress={pickImage}>
-                                <Feather name="image" size={24} color="#ec6a6d" />
-                                <Text style={styles.uploadImageText}>Upload image</Text>
-                            </Pressable>
-                        }
-                        {
-                            image
-                             && 
-                             <View style={styles.selectedImageView}>
-                                <Image source={{ uri: image }} style={styles.selectedImageStyle} /> 
-                             </View>
-                        }
-                    
-                </View>                              
+                    {
+                        !image
+                        &&
+                        <Pressable style={styles.uploadImagePressable} onPress={pickImage}>
+                            <Feather name="image" size={24} color="#ec6a6d" />
+                            <Text style={styles.uploadImageText}>Upload image</Text>
+                        </Pressable>
+                    }
+                    {
+                        image
+                        &&
+                        <View style={styles.selectedImageView}>
+                            <Image source={{ uri: image }} style={styles.selectedImageStyle} />
+                        </View>
+                    }
+
+                </View>
             </View>
-            
-            <Pressable style={styles.signInPressable} onPress={()=>{Alert.alert("You pressed")}}>
-                    <View >
-                        <Text style={styles.signInButtonStyle}>Create Event</Text>
-                    </View>
-            </Pressable> 
+
+            <Pressable style={styles.signInPressable} onPress={() => { Alert.alert("You pressed") }}>
+                <View >
+                    <Text style={styles.signInButtonStyle}>Create Event</Text>
+                </View>
+            </Pressable>
         </View>
     );
 }
-     
+
 
 const styles = StyleSheet.create({
-    parentView : {
-        display : "flex",
+    parentView: {
+        display: "flex",
         flex: 1,
-        flexDirection : "column",
-        justifyContent : "flex-start",
-        alignItems : "center"
+        flexDirection: "column",
+        justifyContent: "flex-start",
+        alignItems: "center"
     },
-    titlesView : {
-        backgroundColor :"#fff",
-        width : 200,
-        height : 50,
-        marginVertical : 60,
-        justifyContent : "space-evenly"
+    titlesView: {
+        backgroundColor: "#fff",
+        width: 200,
+        height: 50,
+        marginVertical: 60,
+        justifyContent: "space-evenly"
     },
     titlesImage: {
         width: 200,
-        height : 80,
-        objectFit : "contain",
+        height: 80,
+        objectFit: "contain",
         marginVertical: 50
     },
-    createCommunityOrEventTextStyle : {
-        fontWeight : "100",
-        marginVertical : 30,
-        marginLeft : '30%',
-        fontSize : 20,
-        fontWeight : "bold",
-        color : '#ec6a6d',
-        height : 60,
-        marginTop : 50
+    createCommunityOrEventTextStyle: {
+        fontWeight: "100",
+        marginVertical: 30,
+        marginLeft: '30%',
+        fontSize: 20,
+        fontWeight: "bold",
+        color: '#ec6a6d',
+        height: 60,
+        marginTop: 50
     },
-    enhanceNewCommunityOrEventTextStyle : {
-        fontStyle : "italic",
-        fontSize : 15,
-        marginTop:20,
+    enhanceNewCommunityOrEventTextStyle: {
+        fontStyle: "italic",
+        fontSize: 15,
+        marginTop: 20,
         marginLeft: 10,
-        marginTop : 20,
+        marginTop: 20,
     },
-    centralView : { 
+    centralView: {
         display: "flex",
         flexDirection: "column",
-        height : '60%',
-        width : 350,
-        marginTop : 5,
-        borderRadius: 40, 
-        backgroundColor : "#A6dACe",
+        height: '60%',
+        width: 350,
+        marginTop: 5,
+        borderRadius: 40,
+        backgroundColor: "#d6e8ff",
     },
-    inputView : {
-        display : "flex",
+    inputView: {
+        display: "flex",
         flexDirection: "row",
-        alignItems : "center",
-        backgroundColor : "#FFF",
+        alignItems: "center",
+        backgroundColor: "#FFF",
         marginTop: 20,
         marginBottom: 10,
         marginHorizontal: 20,
-        height:50,
+        height: 50,
         width: '90%',
-        borderRadius : 15,
-    
+        borderRadius: 15,
+
     },
-    inputPressable : {
-        width : 50,
-        height : 50,
-        backgroundColor : "#fff",
-        borderRadius : 15
+    inputPressable: {
+        width: 50,
+        height: 50,
+        backgroundColor: "#fff",
+        borderRadius: 15
     },
-    inputPressableEnd : {
-        width : 60,
-        height : 50,
-        backgroundColor : "#fff",
-        borderRadius : 15,
-        marginLeft : "auto",
+    inputPressableEnd: {
+        width: 60,
+        height: 50,
+        backgroundColor: "#fff",
+        borderRadius: 15,
+        marginLeft: "auto",
     },
-    inputIcons : { 
-        marginHorizontal:10,
+    inputIcons: {
+        marginHorizontal: 10,
         //objectFit: "contain"
     },
-    inputTextInput : {
-        flex : 1,
-        marginHorizontal:10,
-        height : 40,
-        fontSize : 15
-    },
-    signInPressable : {
-        marginTop : 50,
-        backgroundColor:"#ec6a6d",
+    inputTextInput: {
+        flex: 1,
+        marginHorizontal: 10,
         height: 40,
-        width:"90%",
-        borderRadius : 10,
-        justifyContent : "center",
-        alignItems : "center"
+        fontSize: 15
     },
-    signInButtonStyle : {
-        fontSize : 15,
-        fontWeight : "bold",
-        color : "#fff"
-    },
-    bottomView : {
+    signInPressable: {
+        marginTop: 50,
+        backgroundColor: "#ec6a6d",
+        height: 40,
         width: "90%",
-        marginLeft:"25%",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    signInButtonStyle: {
+        fontSize: 15,
+        fontWeight: "bold",
+        color: "#fff"
+    },
+    bottomView: {
+        width: "90%",
+        marginLeft: "25%",
         display: "flex",
         flexDirection: "row",
         marginTop: 50
     },
-    haveAccount : {
-        fontSize : 15,
-        fontWeight : "bold"
+    haveAccount: {
+        fontSize: 15,
+        fontWeight: "bold"
     },
     connectAccount: {
-        fontSize : 15,
-        fontWeight : "bold",
-        color : "#ec6a6d"
+        fontSize: 15,
+        fontWeight: "bold",
+        color: "#ec6a6d"
     },
-    uploadImageView : {
-        backgroundColor : '#fff',
-        width : '90%',
-        height : '40%',
-        display : 'flex',
-        flexDirection : 'column',
-        justifyContent : 'center',
-        alignItems : 'center',
+    uploadImageView: {
+        backgroundColor: '#fff',
+        width: '90%',
+        height: '40%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
         marginHorizontal: 20,
-        borderRadius : 20,
+        borderRadius: 20,
         // borderStyle : 'dashed',
         // borderWidth :2,
-        borderColor : '#b4a8a5',
+        borderColor: '#b4a8a5',
         marginTop: '5%',
     },
-    uploadImageText : {
-        marginTop : '5%',
-        fontSize : 20,
-        color : '#b4a8a5',
-        fontWeight : 'bold'
+    uploadImageText: {
+        marginTop: '5%',
+        fontSize: 20,
+        color: '#b4a8a5',
+        fontWeight: 'bold'
     },
-    uploadImagePressable : {  
+    uploadImagePressable: {
         backgroundColor: '#fff',
-        display : 'flex',
-        height : 40,
-        width : '100%',
-        flexDirection : 'column',
-        alignItems : 'center',
-        justifyContent : 'center',
-        borderRadius : 5,
-        marginTop : '5%'
+        display: 'flex',
+        height: 40,
+        width: '100%',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        marginTop: '5%'
     },
-    uploadImageIcon : {
+    uploadImageIcon: {
         //objectFit : 'contain',
     },
-    selectedImageView : {
-        width : '100%',
-        height : '100%',
+    selectedImageView: {
+        width: '100%',
+        height: '100%',
     },
-    selectedImageStyle : {
-        resizeMode : 'cover',
-        width : '100%',
-        height : '100%',
-        borderRadius : 20,
+    selectedImageStyle: {
+        resizeMode: 'cover',
+        width: '100%',
+        height: '100%',
+        borderRadius: 20,
     },
-    changeImagePressable : {
-        width : 40,
-        height : 20,
-        backgroundColor : 'transparent',
-        borderRadius : 50,
-        borderColor : '#fff',
-        border : 2
+    changeImagePressable: {
+        width: 40,
+        height: 20,
+        backgroundColor: 'transparent',
+        borderRadius: 50,
+        borderColor: '#fff',
+        border: 2
     },
-    changeImageText : {
-        fontSize : 15,
-        fontStyle : 'italic'
+    changeImageText: {
+        fontSize: 15,
+        fontStyle: 'italic'
     }
 })
